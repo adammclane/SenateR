0977e4e030f5b3acaf6696370650277a0840eaf9
Adam M <adamwmclane@gmail.com>
2016-08-16 07:49:48 -0700

    Update senate.r

--- senate.r
+++ senate.r
@@ -15,19 +15,23 @@ data2$count = 1
 
 #birthday of week setup using lubridate
 data2$birthwday<-wday(data2$objects.person$birthday)
+
 #summarize data using doBy
 wbday<-summaryBy(count~objects.party+birthwday+objects.role_type_label,data=data2,FUN=sum)
+
 #setup weekday plot with ggplot
 ggplot(wbday,aes(x=birthwday,y=count.sum,group=objects.party,color=objects.party))+geom_bar(aes(fill = objects.party),stat="identity",position="dodge")+facet_grid(.~objects.role_type_label)
 
 #birth year setup w/ lubridate
 data2$birthyear<-year(data2$objects.person$birthday)
+
 #summarize with doby
 birthyear<-summaryBy(count~objects.party+birthyear+objects.role_type_label,data=data2,FUN=sum)
+
 #using birthyear, calculate current age
 birthyear$age<-year(now())-birthyear$birthyear
-#plot senate and rep age
 
+#plot senate and rep age
 ggplot(birthyear[birthyear$objects.role_type_label!='President'&birthyear$objects.role_type_label!='Vice President',],aes(x=age,color=objects.party))+geom_density()+facet_grid(.~objects.role_type_label)+ theme(
   panel.border = element_rect(fill = NA, colour = "orange", size = 2)
 )
